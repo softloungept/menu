d3a25b3cd2311633743b125e672d319d4db29614
softloungept <117089042+softloungept@users.noreply.github.com>
2024-03-08 00:47:39 +0000

    hjiouo

--- menu/menu.js
+++ menu/menu.js
@@ -353,7 +353,7 @@ const Menu = [
                             "pt": "(maçã; pera; frutos vermelhos; manga-laranja, pêssego)",
                             "en": "(apple; pear; red berries; mango-orange, peach)"
                         },
-                        "price": 2.5
+                        "price": "3"
                     }
                 ]
             },
